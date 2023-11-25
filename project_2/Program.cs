34853ee3e9b4ad44bbc746389cc77b07253cca1f
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-25 18:22:35 +0300

    project 2 error

---
+++ project_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+int NunFunc(int first){
+    int num = 1;
+    for(int i=1;i<=first;i++){
+        Console.Write(num=num*num, num++ + ',');
+    }
+    return num;
+
+}
+
+
+Console.WriteLine("enter numb");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = NunFunc(num);
+Console.WriteLine(res);
